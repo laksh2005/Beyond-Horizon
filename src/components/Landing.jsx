226aa01acfed2d3bda7cad8f5c1b73491557aa1a
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2025-02-21 12:44:09 +0530

    first commit for this site

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import { Features } from "tailwindcss"
+import MainContainer from "./Landing/MainContainer"
+import Faqs from "./Landing/Faqs"
+
+const Landing = () => {
+  return (
+    <>
+    <MainContainer />
+    <Features />
+    <Faqs />
+    </>
+  )
+}
+
+export default Landing
